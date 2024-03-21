298ccea052da1f1e274d5906ffebd08d33737ff7
xy <xyzmhx@gmail.com>
2024-03-21 11:31:13 +0900

    Add challenge_msg and notice_msg structs and enums to outgoing.rs
    Introduced `challenge_msg` and `notice_msg` structs along with an enum `OutgoingMessageTypes` for better message handling. These changes resulted in adjusting the `send_challenge` and `send_notice` functions within the `OutgoingHandler` trait to return `OutgoingMessageTypes` objects, improving overall code readability and structure.

--- src/msgapi/outgoing.rs
+++ src/msgapi/outgoing.rs
@@ -37,22 +37,56 @@ impl OutgoingMessage {
     }
 }
 
+#[derive(Debug)]
+pub struct challenge_msg {
+    pub challenge_msg: String,
+}
+#[derive(Debug)]
+pub struct notice_msg {
+    pub notice_msg: String,
+}
+
+impl challenge_msg {
+    pub async fn new(challenge_msg: String) -> Self {
+        challenge_msg { challenge_msg }
+    }
+    pub async fn get_challenge_msg(&self) -> String {
+        self.challenge_msg.clone()
+    }
+}
+
+impl notice_msg {
+    pub async fn new(notice_msg: String) -> Self {
+        notice_msg { notice_msg }
+    }
+    pub async fn get_notice_msg(&self) -> String {
+        self.notice_msg.clone()
+    }
+}
+
+#[derive(Debug)]
+pub enum OutgoingMessageTypes {
+    Challenge(challenge_msg),
+    Notice(notice_msg),
+}
 #[async_trait]
 pub trait OutgoingHandler {
-    async fn send_challenge(&self, challenge_msg: String) -> Result<String, Error>;
-    async fn send_notice(&self, notice_msg: String) -> Result<String, Error>;
+    async fn send_challenge(&self, challenge_msg: String) -> Result<OutgoingMessageTypes, Error>;
+    async fn send_notice(&self, notice_msg: String) -> Result<OutgoingMessageTypes, Error>;
 }
 
 #[async_trait]
 impl OutgoingHandler for OutgoingMessage {
-    async fn send_challenge(&self, challenge_msg: String) -> Result<String, Error> {
+    async fn send_challenge(&self, challenge_msg: String) -> Result<OutgoingMessageTypes, Error> {
         let relay_message: RelayMessage = RelayMessage::auth(challenge_msg);
         let challenge_str: String = serde_json::to_string(&relay_message)?;
-        Ok(challenge_str)
+        let ret = challenge_msg::new(challenge_str).await;
+        Ok(OutgoingMessageTypes::Challenge(ret))
     }
-    async fn send_notice(&self, notice_msg: String) -> Result<String, Error> {
+    async fn send_notice(&self, notice_msg: String) -> Result<OutgoingMessageTypes, Error> {
         let relay_message: RelayMessage = RelayMessage::notice(notice_msg);
         let notice_str: String = serde_json::to_string(&relay_message)?;
-        Ok(notice_str)
+        let ret = notice_msg::new(notice_str).await;
+        Ok(OutgoingMessageTypes::Notice(ret))
     }
 }
